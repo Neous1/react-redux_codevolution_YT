da79a742a6f2ff41ef99fb4550c57447caa8d0c5
Yvon N <ynchonon@emoneyadvisor.com>
2019-11-20 03:26:07 -0500

    Add asyncActions file

---
+++ asyncActions.js
@@ -0,0 +1,61 @@
+const redux = require('redux')
+const createStore = redux.createStore
+
+const initialState = {
+    loading: false,
+    users: [],
+    error: ''
+}
+//Actions
+const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST'
+const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS'
+const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE'
+
+//Action creator
+
+//fetch data
+const fetchUsersRequest = () => {
+    return{
+        type: FETCH_USERS_REQUEST
+    }
+}
+//store a list of user if hte request was successful
+const fetchUsersSuccess = (users) => {
+    return{
+        type: FETCH_USERS_SUCCESS,
+        payload: users
+    }
+}
+
+//store error msg if request fails
+const fetchUsersFailure = (error) => {
+    return {
+        type: FETCH_USERS_FAILURE,
+        payload : error
+    }
+}
+
+//define reducer function
+const reducer = (state = initialState, action) =>{
+    switch(action.type){
+        case FETCH_USERS_REQUEST:
+            return{
+                ...state,
+                loading: true
+            }
+        case FETCH_USERS_SUCCESS:
+            return{
+                loading: false,
+                users: action.payload,
+                error: ''
+            }
+        case FETCH_USERS_FAILURE:
+            return{
+                loading: false,
+                users: [],
+                error: action.payload
+            }
+    }
+}
+
+const store = createStore(reducer)
