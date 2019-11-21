badca4f2ee624bb689bb662449f01fbaae7474f9
Yvon N <ynchonon@emoneyadvisor.com>
2019-11-20 22:34:17 -0500

    Add components with hooks

---
+++ react-redux-demo/src/components/HooksCakeContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {buyCake} from '../redux';
+
+function HooksCakeContainer() {
+    const numOfCakes = useSelector(state => state.numOfCakes)
+    const dispatch = useDispatch(buyCake)
+  return (
+    <div>
+        <h2>Num Hooked Cakes - {numOfCakes}</h2>
+        <button onClick={()=>dispatch(buyCake())}>Buy Hooked Cakes</button>
+    </div>
+  );
+}
+
+export default HooksCakeContainer;
